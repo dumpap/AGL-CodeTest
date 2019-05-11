b64e48789c38ce5a7e248de9b51ddc7868faf119
dumpalpr <pradeep.dumpala@outlook.com>
2019-05-11 20:06:49 +0530

    First cut commit

---
+++ src/app/shared/rest-api.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Person } from './person';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RestApiService {
+
+  // Define API
+  apiURL = 'http://agl-developer-test.azurewebsites.net/people.json';
+
+  constructor(private http: HttpClient) { }
+
+  /*========================================
+    CRUD Methods for consuming RESTful API
+  =========================================*/
+
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  // HttpClient API get() method => Fetch person data
+  getPersonWithPets(): Observable<Person> {
+     return this.http.get<Person>(this.apiURL)
+     .pipe(
+       retry(1),
+       catchError(this.handleError)
+     );
+  }
+
+  // Error handling
+  handleError(error) {
+     let errorMessage = '';
+     if (error.error instanceof ErrorEvent) {
+       // Get client-side error
+       errorMessage = error.error.message;
+     } else {
+       // Get server-side error
+       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+     }
+     window.alert(errorMessage);
+     return throwError(errorMessage);
+  }
+
+}
